4d89a55edb2abe0697f44b1cb9da7c16f927f0b8
prakhar kopergaonkar <pkopergaonkar@gmail.com>
2020-07-09 23:16:34 +0530

    random button change

--- Reviews/app.js
+++ Reviews/app.js
@@ -46,7 +46,7 @@ const info = document.getElementById('info');
 
 const prevBtn = document.querySelector('.prev-btn');
 const nextBtn = document.querySelector('.next-btn');
-const randomBtn = document.querySelector('.random-btn');
+const randomBtn = document.querySelector('.btn-random');
 
 // set starting item 
 let currentItem = 0;
@@ -66,17 +66,19 @@ function showPerson(person) {
 }
 
 prevBtn.addEventListener('click',function() {
-  if(currentItem>0) {
     currentItem--;
+    if(currentItem<0) {
+      currentItem = reviews.length-1;
+    }
     showPerson(currentItem)
-  }
 })
 
 nextBtn.addEventListener('click',function() {
-  if(currentItem<3) {
-    currentItem++;
-    showPerson(currentItem)
+  currentItem++;
+  if(currentItem>3) {
+    currentItem=0
   }
+  showPerson(currentItem)
 })
 
 randomBtn.addEventListener('click',function() {
