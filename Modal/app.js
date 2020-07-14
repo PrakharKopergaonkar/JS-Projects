10723ec2cb420142c0851c167428d8d43bf154a2
prakhar kopergaonkar <pkopergaonkar@gmail.com>
2020-07-14 23:33:18 +0530

    modal update

--- Modal/app.js
+++ Modal/app.js
@@ -2,9 +2,9 @@
 // listen for click events on modal-btn and close-btn
 // when user clicks modal-btn add .open-modal to modal-overlay
 // when user clicks close-btn remove .open-modal from modal-overlay
-const modal_btn = document.getElementsByClassName('btn modal-btn')[0];
-const modal_overlay = document.getElementsByClassName('modal-overlay')[0];
-const close_btn = document.getElementsByClassName('close-btn')[0];
+const modal_btn = document.querySelector('.modal-btn');
+const modal_overlay = document.querySelector('.modal-overlay');
+const close_btn = document.querySelector('.close-btn');
 
 modal_btn.addEventListener('click',function() {
     modal_overlay.classList.add('open-modal')
